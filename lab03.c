06dca4a48c6b308d110d1ddbb51e5ed263bbbe28
shuchin <shuchin@shuchin-VirtualBox.(none)>
2016-09-19 17:42:10 +0800

    lab03

---
+++ lab03.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int a0,a1,a2,a3,a4,a5,a6,a7,a8,a9;
+printf(""\n);
+scanf("%d %d %d %d %d %d %d %d %d %d",&a0,&a1,&a2,&a4,&a5,&a6,&a7,&a8,&a9);
+printf(""\n);
+printf("%d"\n,a0);
+printf("%d"\n,a1);
+printf("%d"\n,a2);
+printf("%d"\n,a3);
+printf("%d"\n,a4);
+printf("%d"\n,a5);
+printf("%d"\n,a6);
+printf("%d"\n,a7);
+printf("%d"\n,a8);
+printf("%d"\n,a9);
+}
