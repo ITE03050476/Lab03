80ee7cbfbd5e05876c002ffd88f03a64be1adc05
shuchin <shuchin@shuchin-VirtualBox.(none)>
2016-09-26 10:08:12 +0800

    make

---
+++ lab.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int input;
+int number;
+printf("input 10 numbers:\n");
+for(number=0;number<=10;number++)
+{
+scanf("%d",&input);
+printf("number is:%d\n",input);
+}
+return 0;
+}
